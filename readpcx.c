d77ceb46f196f1866515524ed6d814eb5108268a
QuzarDC <quzar@completext.com>
2025-09-23 21:27:39 -0400

    Update to use stdint types.
    This was the last piece of in-house software that was using
    the `<arch/types.h>` types implicitly via the way they were
    being inappropriately added to `<sys/_types.h>`.

--- readpcx.c
+++ readpcx.c
@@ -12,38 +12,38 @@ typedef struct {
 	char   ver;               /* encoder version number (5)		*/
 	char   enc;               /* encoding code, always 1		*/
 	char   bpp;               /* bits per pixel, 8 in mode 0x13	*/
-	uint16 xmin,ymin;         /* image origin, usually 0,0		*/
-	uint16 xmax,ymax;         /* image dimensions			*/
-	uint16 hres;              /* horizontal resolution value	*/
-	uint16 vres;              /* vertical resolution value		*/
+	uint16_t xmin,ymin;       /* image origin, usually 0,0		*/
+	uint16_t xmax,ymax;       /* image dimensions			*/
+	uint16_t hres;            /* horizontal resolution value	*/
+	uint16_t vres;            /* vertical resolution value		*/
 	char   pal[48];           /* palette (not in mode 0x13)		*/
 	char   reserved;          /* who knows?				*/
 	char   clrplanes;         /* number of planes, 1 in mode 0x13	*/
-	uint16 bpl;               /* bytes per line, 80 in mode 0x13	*/
-	uint16 pltype;            /* Grey or Color palette flag		*/
+	uint16_t bpl;             /* bytes per line, 80 in mode 0x13	*/
+	uint16_t pltype;          /* Grey or Color palette flag		*/
 	char   filler[58];        /* Zsoft wanted a 128 byte header	*/
 } pcx_hdr;
 
 FILE *pcxfile;
 
-uint32 readpcx_init(FILE *infile)
+uint32_t readpcx_init(FILE *infile)
 {
   pcxfile = infile;
   return 0;
 }
 
-uint8 *readpcx_get_image(uint32 *pChannels, uint32 *pRowBytes,
-                         uint32 *pWidth, uint32 *pHeight)
+uint8_t *readpcx_get_image(uint32_t *pChannels, uint32_t *pRowBytes,
+                           uint32_t *pWidth, uint32_t *pHeight)
 {
-  pcx_hdr header;
-  uint32 bytesRead;
-  uint8 *preImage;
-  uint8 *image;
-  uint8 *palette;
-  uint32 numBytes;
-  uint32 i;
-  uint8 pixel;
-  uint8 runlen;
+  pcx_hdr  header;
+  uint32_t bytesRead;
+  uint8_t *preImage;
+  uint8_t *image;
+  uint8_t *palette;
+  uint32_t numBytes;
+  uint32_t i;
+  uint8_t  pixel;
+  uint8_t  runlen;
   
   bytesRead = fread(&header, sizeof(pcx_hdr), 1, pcxfile);
 
@@ -60,12 +60,12 @@ uint8 *readpcx_get_image(uint32 *pChannels, uint32 *pRowBytes,
 
   for(i = 0; i < numBytes;)
   {
-    fread(&pixel, sizeof(uint8), 1, pcxfile);
+    fread(&pixel, sizeof(uint8_t), 1, pcxfile);
 
     if (pixel & 0xc0)
     {
       runlen = (pixel & 0x3f);
-      fread(&pixel, sizeof(uint8), 1, pcxfile);
+      fread(&pixel, sizeof(uint8_t), 1, pcxfile);
       while(runlen--)
         preImage[i++] = pixel;
     }
@@ -74,7 +74,7 @@ uint8 *readpcx_get_image(uint32 *pChannels, uint32 *pRowBytes,
 
   }
 
-  fread(&pixel, sizeof(uint8), 1, pcxfile);
+  fread(&pixel, sizeof(uint8_t), 1, pcxfile);
   palette = malloc(768);
 
   fread(palette, 768, 1, pcxfile);
